dd68be62e09966d00186a8cb0f07830e4f041384
Shashi Kumar <shashiessp@gmail.com>
2015-09-26 02:35:45 +0530

    minor fixes

--- news/forms.py
+++ news/forms.py
@@ -3,7 +3,6 @@ from news.models import Article, Vote, Read, Delete
 
 
 class ArticleForm(ModelForm):
-
 	class Meta(ModelForm):
 		model = Article
 		fields = ('title', 'url', 'description')
@@ -11,6 +10,8 @@ class ArticleForm(ModelForm):
 					'posted_by', 'comments', 'up_votes' ,'posted_on')
 
 	def __init__(self, *args, **kwargs):
+		""" add bootstrap css classes to forms
+		"""
 		super(ArticleForm, self).__init__(*args, **kwargs)
 		self.fields['title'].widget.attrs.update({'class' : 'form-control input', 'placeholder':'Title'})
 		self.fields['url'].widget.attrs.update({'class' : 'form-control', 'placeholder':'link'})
